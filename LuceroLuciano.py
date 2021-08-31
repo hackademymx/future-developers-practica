3a5b705d02747a2ffd532c22af708d2aaa5587da
LuceroLuciano <lucero.luciano@umb.edu.mx>
2021-08-30 23:17:56 -0500

    Se agrego el archivo LuceroLuciano.py

---
+++ LuceroLuciano.py
@@ -0,0 +1,10 @@
+# Numero par o impar
+# Lab2 - FutureDeveloper
+
+numero = int(input('Ingresa un numero: ')
+
+for i in range(1, numero + 1):
+	if i % 2 == 0:
+		print(i, 'Es numero PAR')
+	else:
+		print(i, 'Es numero IMPAR')
