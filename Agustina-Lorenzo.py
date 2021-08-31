a7cbf58faf8c6af4730dd1b5810bbcf11c09c2a3
AgustinaLo <magustina.lorenzo@gmail.com>
2021-08-31 19:48:16 -0300

    Añadido ejercicio de algoritmos

---
+++ Agustina-Lorenzo.py
@@ -0,0 +1,17 @@
+dia= int(input("Ingrese un numero: "))  
+if dia == 1:
+    print ("Lunes")
+elif dia == 2:
+    print ("Martes")
+elif dia == 3:
+    print ("Miercoles")
+elif dia == 4:
+    print ("Jueves")
+elif dia == 5:
+    print ("Viernes")
+elif dia == 6:
+    print ("Sabado")
+elif dia == 7:
+    print ("Domingo")
+else:
+    print ("El numero es invalido")
